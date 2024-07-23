35ab87f386a2884877086030802b328107f75e98
MIsrarAhmad <m.israrahmad2021@gmail.com>
2024-07-22 21:26:13 -0700

    assignment 9 done

---
+++ script.js
@@ -0,0 +1,52 @@
+// function addNumber(num1, num2) {
+//     return num1 + num2;
+// }
+// console.log(addNumber(2, 3));
+// console.log(addNumber(5, 1));
+// console.log(addNumber(10, 20));
+
+// function toUpper(str) {
+//     return str.toUpperCase();
+// }
+
+// console.log(toUpper("da zama mehnat de"));
+// console.log(toUpper("Lodhi"));
+
+// Assignment 3
+// function averageArray(arr) {
+//     if (arr.length === 0) return 0;
+//     const sum = arr.reduce((acc, num) => acc + num, 0);
+//     return sum / arr.length;
+// }
+// console.log(averageArray([1, 2, 3]));
+// console.log(averageArray([5, 5, 5]));
+// console.log(averageArray([10, 20, 30]));
+
+// Assignment 4
+// function removeVowels(str) {
+//     return str.replace(/[aeiouAEIOU]/g, '')
+// }
+// console.log(removeVowels("israr"));
+// console.log(removeVowels("israrlodhi"));
+// console.log(removeVowels("Asad"))
+
+// Assignment 5
+// function factorial(n) {
+//     if (n < 0) return -1;
+//     if (n === 0) return 1;
+//     return n * factorial(n - 1);
+// }
+// console.log(factorial(5));
+// console.log(factorial(3));
+// console.log(factorial(30));
+
+// Assignment 6
+// function reverseString(str) {
+//     return str.split('').reverse().join('');
+// }
+
+// console.log(reverseString("Misar"));
+// console.log(reverseString("Misrarahmad"));
+// console.log(reverseString("Misrarahmad"));
+
+
